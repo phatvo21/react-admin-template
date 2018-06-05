b193030eabda6a7ae026ed06e10695b52d35e3a0
vo tan phat <tanvp@soft-flight.work>
2018-06-05 09:36:02 +0700

    #all this is all commit

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+          <Button variant="contained" color="secondary">
+              Hello World
+          </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
